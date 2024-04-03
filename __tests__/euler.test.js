7b5c9becbc3eb98164715ea3ca2a4b06a07e8d7a
Aidan Williams <aidanwilliams01@gmail.com>
2024-04-03 14:37:28 -0700

    add code and passing tests

--- __tests__/euler.test.js
+++ __tests__/euler.test.js
@@ -1,8 +1,19 @@
-import { multiples } from "../src/euler";
+import { multiples, fibonacci } from "../src/euler";
 
 describe('multiples', () => {
 
   test('should return a number', () => {
     expect(typeof multiples()).toEqual('number');
   });
+
+  test('should return the sum of the multiples of 3 or 5 below a given argument', () => {
+    expect(multiples(1000)).toEqual(233168);
+  });
 })
+
+describe('fibonacci', () => {
+
+  test('should return a number', () => {
+    expect(typeof fibonacci()).toEqual('number');
+  });
+});
--- src/euler.js
+++ src/euler.js
@@ -1,3 +1,26 @@
-export function multiples() {
+export function multiples(sumTo) {
+  let sum = 0;
+  let number = 0;
+
+  function checkNumber() {
+    if (number % 3 === 0) {
+      sum += number;
+    }
+    else if (number % 5 === 0) {
+      sum += number;
+    }
+    number += 1;
+    if (number < sumTo) {
+      return checkNumber();
+    }
+    else {
+      return sum;
+    }
+  }
+  
+  return checkNumber();
+}
+
+export function fibonacci() {
   return 0;
 }
