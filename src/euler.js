b59afc1632e2c09c8354cd1137a44fffe88a10d7
Aidan Williams <aidanwilliams01@gmail.com>
2024-04-03 15:14:16 -0700

    add passing code

--- src/euler.js
+++ src/euler.js
@@ -21,6 +21,37 @@ export function multiples(sumTo) {
   return checkNumber();
 }
 
-export function fibonacci() {
-  return 0;
+export function fibonacci(sumTo) {
+  const fibonacciArray = [1, 2];
+  let index = 0;
+  let sum = 0;
+  
+  function populateArray() {
+    const element = fibonacciArray[index];
+    const element2 = fibonacciArray[index + 1];
+    const newElement = element + element2;
+    fibonacciArray.push(newElement);
+    index += 1;
+    if (newElement <= sumTo) {
+      populateArray()
+    }
+    else {
+      index = 0;
+    }
+  }
+
+  function sumEvenValues() {
+    const element = fibonacciArray[index];
+    if (index === fibonacciArray.length) {
+      return sum;
+    }
+    if (element % 2 === 0) {
+      sum += element;
+    }
+    index += 1;
+    return sumEvenValues();
+  }
+  
+  populateArray()
+  return sumEvenValues()
 }
